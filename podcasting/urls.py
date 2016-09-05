ccb5300f2b5df8d372e6a89ab7460a4b07a57470
Simon Wörpel <simon.woerpel@medienrevolte.de>
2016-09-05 19:03:56 +0200

    Include `urls_feeds` into main `urls` and add `cms_apps.py`

--- podcasting/urls.py
+++ podcasting/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from podcasting.views import ShowListView, ShowDetailView, EpisodeListView, EpisodeDetailView
 
 
@@ -7,6 +7,7 @@ urlpatterns = patterns(
 
     url(r"^$", ShowListView.as_view(),
         name="podcasting_show_list"),
+    url(r"^feeds/", include('podcasting.urls_feeds')),
     url(r"^(?P<slug>[-\w]+)/$", ShowDetailView.as_view(),
         name="podcasting_show_detail"),
     url(r"^(?P<show_slug>[-\w]+)/archive/$", EpisodeListView.as_view(),
